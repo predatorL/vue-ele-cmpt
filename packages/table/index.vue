a5c0589271e8ae30750df9f77270058a91cfd2b2
lihaiyang <lihaiyang@linkedsee.com>
2018-01-29 11:01:26 +0800

    调整table组件

--- packages/table/index.vue
+++ packages/table/index.vue
@@ -1,22 +1,21 @@
 <template>
     <el-table :data="tableData" style="width: 100%">
-        <template v-for="(item, index) in tableProps" :prop="item.prop" :label="item.label" :width="item.width">
-            <el-table-column :key="index" v-if="item.filter" :label="item.label" :width="item.width">
-                <template slot-scope="scope">
-                    {{item.filter(scope.row[item.prop], scope.row, scope)}}
-                </template>
-            </el-table-column>
-            <el-table-column :key="index" v-else-if="item.actions" :label="item.label" :width="item.width">
-                <template slot-scope="scope">
-                    <el-button v-for="(action, btnIndex) in item.actions"
+        <template v-for="(item, index) in tableProps" >
+            <el-table-column :key="index" v-if="item.actions" :label="item.label" :width="item.width">
+                <template scope="scope">
+                    <el-button :title="action.title" v-for="(action, btnIndex) in item.actions"
                         :key="btnIndex" size="small" type="primary"
                         @click="onClick(action.method, scope.row, scope)"
-                        :icon="action.icon" :title="action.title">
-                        {{action.text}}
+                        :icon="action.icon">
                     </el-button>
                 </template>
             </el-table-column>
-            <el-table-column :key="index" v-else :prop="item.prop" :label="item.label" :width="item.width">
+            <el-table-column :key="index" v-else :label="item.label" :width="item.width">
+                <template scope="scope">
+                    <span :title="scope.row[item.prop]">
+                        {{item.filter ? item.filter(scope.row[item.prop], scope.row, scope) : scope.row[item.prop]}}
+                    </span>
+                </template>
             </el-table-column>
         </template>
     </el-table>
